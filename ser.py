c0432c2842f5676b8d35e5751ba8d8a27721d138
Артём <artjomereomin2@yandex.ru>
2020-04-08 11:29:53 +0400

    ser

---
+++ ser.py
@@ -0,0 +1,11 @@
+a = list(map(int, input().split()))
+b = input()
+c = float(input())
+d = True
+e = None
+simple = dict(list=a,
+              text=b,
+              number=c,
+              boolean=d,
+              none=e)
+print(simple)
